d0f7acfe99c12d1fb7b90f631d3eddb467101d8e
Umar Farooq <umarfarooqshafi2101@gmail.com>
2024-05-07 22:29:56 +0500

    strategy

--- src/url/views.py
+++ src/url/views.py
@@ -4,10 +4,25 @@ from .models import *
 import logging
 from decouple import config
 
-from strategy import RunForeverWithBreaks
-
+from utils.bot import AutoLikeBot
+from utils.filter import MyCustomFilter
+from utils.strategy import RunForeverWithBreaks
 
 from django.contrib import messages
+
+def configure_chrome_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument(f"user-data-dir={pathlib.Path(__file__).parent.absolute().joinpath('chrome-profile')}")
+
+    # disable image loading for better performance
+    # options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
+
+    the_driver = webdriver.Chrome(executable_path=config.DRIVER_EXECUTABLE_PATH, options=options)
+
+    # page loading time and wait time for page reload
+    the_driver.set_page_load_timeout(5)
+    the_driver.implicitly_wait(2)
+    return the_driver
 
 def scrap(request):
     with AutoLikeBot(configure_chrome_driver(),
